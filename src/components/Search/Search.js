4a83c522d3ccdcf4ef3ca3ad756a7a7efb32e432
Chayma-ab <abidallicha@gmail.com>
2024-07-11 16:02:05 +0100

    implementing bootstrap drodown

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,26 +1,40 @@
 import React from 'react';
 import './search.css';
 
+import { Dropdown } from 'react-bootstrap';
+
 function Search({ shapes, selectedShapes, setSelectedShapes }) {
     const handleChange = (event) => {
-        const options = event.target.options;
-        const selected = [];
-        for (let i = 0, l = options.length; i < l; i++) {
-            if (options[i].selected) {
-                selected.push(options[i].value);
-            }
-        }
-        setSelectedShapes(selected);
+        // const options = event.target.options;
+        // const selected = [];
+        // for (let i = 0, l = options.length; i < l; i++) {
+        //     if (options[i].selected) {
+        //         selected.push(options[i].value);
+        //     }
+        // }
+        // setSelectedShapes(selected);
+        console.log("change value of dropdown")
     };
 
     return (
         <div>
-            <select multiple={true} value={selectedShapes} onChange={handleChange}>
+            {/* <select multiple={true} >
                 <option value="all">All</option>
                 {shapes.map((shape, key) => (
                     <option key={key} value={shape.name}>{shape.name}</option>
                 ))}
-            </select>
+            </select> */}
+            <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    Dropdown Button
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu value={selectedShapes} onChange={handleChange}>
+                    {shapes.map((shape, key) => (
+                        <Dropdown.Item value={shape.name}>{shape.name}</Dropdown.Item>
+                    ))}
+                </Dropdown.Menu>
+            </Dropdown>
         </div>
     );
 }
